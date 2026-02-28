8fca4807c60c5d28e1bb6075af54bcd116be7656
Sherin Thomas <sherint275@gmail.com>
2026-03-01 00:17:18 +0530

    fix cors

--- Web-Escape-Backend/server.js
+++ Web-Escape-Backend/server.js
@@ -11,6 +11,12 @@ import gameRoutes from "./routes/gameRoutes.js";
 import levelRoutes from "./routes/levelRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 
+const corsOption = {
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true,
+};
+
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,13 +29,6 @@ app.get("/home", (req, res) => {
     res.send("i am coming from backend");
 });
 
-const corsOption = {
-    origin: "http://localhost:5173",
-    credentials: true,
-};
-
-app.use(cors(corsOption));
-
 app.use("/api/v1/user", authRoutes);
 app.use("/api/v1/game", gameRoutes);
 app.use("/api/v1/level", levelRoutes);
@@ -38,6 +37,5 @@ app.use("/api/v1/upload", uploadRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     connectDb();
-    console.log("app is listening on port 3000");
+    console.log(`app is listening on port ${PORT}`);
 });
-
